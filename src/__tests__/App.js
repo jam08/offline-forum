be0911bc138b0a4e33f0502c15d6fd4627cc2d45
Janaina Marana <jmarana@Janainas-MacBook-Pro.local>
2018-01-22 13:38:40 +0100

    added test cases

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, shallow } from 'enzyme';
 import App from '../components/App';
+import * as api from '../api/index';
 
-test('renders the app', () => {
-  render(<App />);
+it('loads the app', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.mt-8')).to.have.length(1);
 });
+
+it('let select a persona');
+it('loads posts');
+it('loads comments');
+it('x button visible for post author');
+it('no x button on other users posts');
+it('input component takes users input');
+it('should add new post when create button is clicked');
+it('should add comment when comment button is clicked');
+it('should delete post when x button is clicked');
+it('loads bot page when talk to real human button is clicked');
+it('get a reply from bot after sicka button is clicked');
+it('sicka button should be disable if no message');
